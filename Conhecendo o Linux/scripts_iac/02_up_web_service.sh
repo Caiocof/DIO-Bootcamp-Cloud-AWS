0464f807e34d81805834cc9cbf3bafff8862638f
caiocof <caiooliveira3652@gmail.com>
2023-03-30 12:16:26 -0300

    refact: adjusts in iac-apache

--- Conhecendo o Linux/scripts_iac/02_up_web_service.sh
+++ Conhecendo o Linux/scripts_iac/02_up_web_service.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "Update sistem"
-sudo apt update -y
+apt update -y
 
-echo "Install git"
-sudo apt install git -y
+echo "Install git and apache2"
+apt install git -y && apt install apach2 -y
 
-echo "Install apache2"
-sudo apt install apach2 -y
+echo "Remove project folder"
+rm -rf DevOps
 
 echo "Clone repository"
 git clone https://github.com/Caiocof/DevOps.git
 
 echo "Copy folter for Apache service"
-sudo cp -r /DevOps/project_01/html-template/* /var/www/html/
+sudo cp -r DevOps/project_01/html-template/* /var/www/html/
 
 
-echo "finish..."
+echo "script finish..."
